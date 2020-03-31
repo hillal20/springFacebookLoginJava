557f121bb822697c28c6df25c0000e3c6ee7b03d
Hilal Aissani <hilalaissani@gmail.com>
2020-03-31 18:04:24 +0100

    latest

---
+++ src/main/java/springscurityfacebooklogin/springfacebooklogin/SpringFaceBookLoginApplication.java
@@ -0,0 +1,14 @@
+package springscurityfacebooklogin.springfacebooklogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFaceBookLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFaceBookLoginApplication.class, args);
+        System.out.println(" == spring app is running ===");
+    }
+
+}
---
+++ src/test/java/springscurityfacebooklogin/springfacebooklogin/SpringfacebookloginApplicationTests.java
@@ -0,0 +1,13 @@
+package springscurityfacebooklogin.springfacebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfacebookloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
