a63db44af64d2a2d2bceb89e8733313913b92f37
Hilal Aissani <hilalaissani@gmail.com>
2020-04-02 19:32:16 +0100

    latest

--- src/main/java/springscurityfacebooklogin/springfacebooklogin/SpringFaceBookLoginApplication.java
+++ src/main/java/springscurityfacebooklogin/springfacebooklogin/SpringFaceBookLoginApplication.java
@@ -2,8 +2,10 @@ package springscurityfacebooklogin.springfacebooklogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso // to enable single singOn oauth
 public class SpringFaceBookLoginApplication {
 
     public static void main(String[] args) {
